37325de0f17efed556a78326768c5f5a1e50dc1d
b0b3t0 <bnenov91@gmail.com>
2021-04-15 18:58:14 +0300

    Added task for removing duplicates elements from array and returning the remaining count

---
+++ removeDuplicatesElements/src/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    /**
+     * Removing  all instances of that value in place.
+     * @param nums input array
+     * @param val search value to remove
+     * @return the count of elements that are not equal to value
+     */
+    public static int removeDuplicatesElements(int[] nums, int val) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 3 , 3, 2};
+        System.out.println(removeDuplicatesElements(nums, 3));
+    }
+}
