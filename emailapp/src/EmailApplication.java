f96b90bd4b1b40bd76ee0cbf9095a3580d18770d
b0b3t0 <bnenov91@gmail.com>
2021-04-18 22:16:54 +0300

    Added methods: toString, setDepartment and initialize mailboxcapacity with default value of 1000

--- emailapp/src/EmailApplication.java
+++ emailapp/src/EmailApplication.java
@@ -1,6 +1,7 @@
 public class EmailApplication {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Email em = new Email("John", "Cena");
+        System.out.println(em);
     }
 }
