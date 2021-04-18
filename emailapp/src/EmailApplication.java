f4bfde2e44011299ee43310adb1cd0043e550cd1
b0b3t0 <bnenov91@gmail.com>
2021-04-18 22:42:13 +0300

    Added constant for company.org value instead of leaving it magic string in the setEmail method

--- emailapp/src/EmailApplication.java
+++ emailapp/src/EmailApplication.java
@@ -3,5 +3,6 @@ public class EmailApplication {
     public static void main(String[] args) {
         Email em = new Email("John", "Cena");
         System.out.println(em);
+        System.out.println(em.getEmail());
     }
 }
