d9f662bcbe34af0f24dc02061175f133dccc8aaa
b0b3t0 <bnenov91@gmail.com>
2021-04-18 22:31:35 +0300

    Added method for displaying email in syntax: firstname.lastname@department.company.com

--- emailapp/src/EmailApplication.java
+++ emailapp/src/EmailApplication.java
@@ -2,6 +2,8 @@ public class EmailApplication {
 
     public static void main(String[] args) {
         Email em = new Email("John", "Cena");
+        em.changePassword("asd123zxc123");
         System.out.println(em);
+        System.out.println(em.printEmail());
     }
 }
