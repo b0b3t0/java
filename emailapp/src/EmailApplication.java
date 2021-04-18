72abec8dc15b01874cacc26ba5f4210deabb83fe
b0b3t0 <bnenov91@gmail.com>
2021-04-18 22:39:35 +0300

    Removed parameter from method generateRandomPassword and used the default password lenght constant in the method

--- emailapp/src/EmailApplication.java
+++ emailapp/src/EmailApplication.java
@@ -2,8 +2,6 @@ public class EmailApplication {
 
     public static void main(String[] args) {
         Email em = new Email("John", "Cena");
-        em.changePassword("asd123zxc123");
         System.out.println(em);
-        System.out.println(em.getEmail());
     }
 }
